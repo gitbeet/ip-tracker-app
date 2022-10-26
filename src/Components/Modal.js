07b63754f2b69ea8094cf75003c9d05de1c7e242
gitbeet <nikig715@gmail.com>
2022-10-26 17:45:37 +0200

    added enter and esc functionality for modal and searchbar

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Backdrop from "./Backdrop";
 import "../css/Modal.css";
 
 export default function Modal({ show, onClose, message }) {
+  useEffect(() => {
+    const listener = (event) => {
+      if (
+        event.code === "Enter" ||
+        event.code === "NumpadEnter" ||
+        event.code === "Escape"
+      ) {
+        console.log("Enter key was pressed. Run your function.");
+        event.preventDefault();
+        onClose();
+      }
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, []);
+
   if (!show) return null;
   return (
     <>
--- src/Components/SearchComponent.js
+++ src/Components/SearchComponent.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../css/SearchComponent.css";
 import iconArrow from "../images/icon-arrow.svg";
 
 export default function SearchComponent({ handleChange, handleSubmit }) {
+  useEffect(() => {
+    const listener = (event) => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        console.log("Enter key was pressed. Run your function.");
+        event.preventDefault();
+        handleSubmit();
+      }
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, []);
+
   return (
     <div className="search-component">
       <input placeholder="Search IP ..." onChange={handleChange} />
