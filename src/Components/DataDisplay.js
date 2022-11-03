ff8b3d03b657a3c61114884ebf16f8511e7141d6
gitbeet <nikig715@gmail.com>
2022-11-03 14:30:28 +0100

    fixing data display window adding transition animation

--- src/Components/DataDisplay.js
+++ src/Components/DataDisplay.js
@@ -7,73 +7,72 @@ import { useDarkMode } from "../context/darkModeContext";
 export default function DataDisplay({ data }) {
   const { darkMode } = useDarkMode();
   const [minimized, setMinimized] = useState(true);
-  function toggleMinimize() {
-    setMinimized((prev) => !prev);
-  }
+
   if (!data) return <h1>loading...</h1>;
   return (
     <div
+      onClick={() => setMinimized((prev) => !prev)}
       className={
         minimized
           ? `data-display-minimized${darkMode ? "-dark-mode" : ""}`
           : `data-display${darkMode ? "-dark-mode" : ""}`
       }
     >
-      <div
-        className={
-          minimized
-            ? "data-display-minimize-button-minimized"
-            : "data-display-minimize-button"
-        }
-      >
-        <MinimizeButton minimized={minimized} onClick={toggleMinimize} />
-      </div>
-      <div className={minimized ? "hidden" : " data-container"}>
-        <p className="data-container-text-small">IP ADDRESS</p>
-        <p
-          className={`data-container-text-medium${
-            darkMode ? "-dark-mode" : ""
-          }`}
-        >
-          {data.ip}
-        </p>
-      </div>
-      <div
-        className={minimized ? "data-container-minimized" : "data-container"}
-      >
-        <p className="data-container-text-small">LOCATION</p>
+      <div className="data-display-container">
         <div
-          className={`data-container-text-medium${
-            darkMode ? "-dark-mode" : ""
-          }`}
-          pan
+          onClick={() => setMinimized((prev) => !prev)}
+          className="data-display-minimize-button"
         >
-          {data.location.city},
-          {(states.find((state) => state[0] === data.location.region) &&
-            states.find((state) => state[0] === data.location.region)[1]) ||
-            data.location.region}{" "}
-          {data.location.postalCode}
+          <MinimizeButton
+            onClick={() => setMinimized((prev) => !prev)}
+            minimized={minimized}
+          />
         </div>
-      </div>
-      <div className={minimized ? "hidden" : " data-container"}>
-        <p className="data-container-text-small">TIMEZONE</p>
-        <p
-          className={`data-container-text-medium${
-            darkMode ? "-dark-mode" : ""
-          }`}
-        >
-          UTC{data.location.timezone}
-        </p>
-      </div>
-      <div className={minimized ? "hidden" : " data-container"}>
-        <p className="data-container-text-small">ISP</p>
-        <p
-          className={`data-container-text-medium${
-            darkMode ? "-dark-mode" : ""
-          }`}
-        >
-          {data.isp || ""}
-        </p>
+        <div className={!minimized ? "data-container" : "data-container"}>
+          <p className="data-container-text-small">LOCATION</p>
+          <div
+            className={`data-container-text-medium${
+              darkMode ? "-dark-mode" : ""
+            }`}
+            pan
+          >
+            {data.location.city},
+            {(states.find((state) => state[0] === data.location.region) &&
+              states.find((state) => state[0] === data.location.region)[1]) ||
+              data.location.region}{" "}
+            {data.location.postalCode}
+          </div>
+        </div>
+        <div className={minimized ? "data-container" : " data-container"}>
+          <p className="data-container-text-small">IP ADDRESS</p>
+          <p
+            className={`data-container-text-medium${
+              darkMode ? "-dark-mode" : ""
+            }`}
+          >
+            {data.ip}
+          </p>
+        </div>
+        <div className={minimized ? "data-container" : " data-container"}>
+          <p className="data-container-text-small">TIMEZONE</p>
+          <p
+            className={`data-container-text-medium${
+              darkMode ? "-dark-mode" : ""
+            }`}
+          >
+            UTC{data.location.timezone}
+          </p>
+        </div>
+        <div className={minimized ? "data-container" : " data-container"}>
+          <p className="data-container-text-small">ISP</p>
+          <p
+            className={`data-container-text-medium${
+              darkMode ? "-dark-mode" : ""
+            }`}
+          >
+            {data.isp || ""}
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/Components/MinimizeButton.js
+++ src/Components/MinimizeButton.js
@@ -3,12 +3,12 @@ import { FiMinimize2, FiMaximize2 } from "react-icons/fi";
 
 export default function MinimizeButton({ minimized, onClick }) {
   return (
-    <div onClick={onClick}>
+    <>
       {minimized ? (
-        <FiMaximize2 size="1.5rem" />
+        <FiMaximize2 size="1.5rem" onClick={onClick} />
       ) : (
-        <FiMinimize2 size="1.5rem" />
+        <FiMinimize2 size="1.5rem" onClick={onClick} />
       )}
-    </div>
+    </>
   );
 }
